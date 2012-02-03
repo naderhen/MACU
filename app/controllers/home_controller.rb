ff604eee9953209f07a761b61dfabdc850457987
Nader Hendawi <naderhen@gmail.com>
2012-02-03 08:42:53 -0500

    q

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 	
 	def index
 		@users = User.all
-		@questions = Question.all.sample(1)
+		@questions = Question.all.sample(6)
 		@feeds = Feed.all
 		@indices = {
 			1 => 'a',
