15b1cae13acfd30a55ef89ff38be4888faebd134
Nader Hendawi <naderhen@gmail.com>
2012-02-07 20:25:25 -0500

    last?

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 	
 	def index
 		@users = User.all
-		@questions = Question.all.sample(6)
+		@questions = Question.all.sample(12)
 		@feeds = Feed.all
 		@indices = {
 			1 => 'a',
