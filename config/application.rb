078609839d948c66e3ba6b56af408aff7cc5ad7b
Nader Hendawi <naderhen@gmail.com>
2012-02-09 18:25:47 -0500

    ssl

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module Macu
       g.helper_specs false
     end
 
+    config.force_ssl = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
